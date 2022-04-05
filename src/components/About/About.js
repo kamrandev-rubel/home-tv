58b5a87d94deb50f3c1198ae8d3901d67ccae99b
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-04-05 03:48:58 +0300

    added about page

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import imgSoon from '../../images/coming-soon.png'
+
+const About = () => {
+    return (
+        <div>
+            <img src={imgSoon} alt="" className='w-full' />
+        </div>
+    );
+};
+
+export default About;
